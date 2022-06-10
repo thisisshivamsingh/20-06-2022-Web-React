a9ea328cca391c8ae4abc7557136c902298866cb
Shivam <thisisshivam06@gmail.com>
2022-06-10 20:31:54 +0530

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import User from "./User";
+import User from "./User";
 
 function App() {
+  function Apple() {
+    return <div>Apple Component</div>;
+  }
   return (
     <div className="App">
       <h1>Hello World !</h1>
       <User />
-      <User />
+      <Apple />
     </div>
   );
 }
-function User() {
-  return (
-    <div>
-      <h1>User Component</h1>
-      <h1>Hello</h1>
-    </div>
-  );
-}
+
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,3 +1,6 @@
-
-
-// export default User;
+import React, { Component } from "react";
+export default class User extends Component {
+  render() {
+    return <h1>Hello from User</h1>;
+  }
+}
