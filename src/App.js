74205082f353bde5ab4c3cda63646d7b55c249f2
Shivam <thisisshivam06@gmail.com>
2022-06-12 16:57:45 +0530

    Basic Form Validation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Profile from "./Profile";
+import Login from "./Login";
 
 function App() {
+  const data = true;
   return (
     <div className="App">
-      <Profile />
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [userErr, setUserErr] = useState(false);
+  const [passErr, setPassErr] = useState(false);
+  function loginHandle(e) {
+    if (user.length < 3 || password.length < 3) {
+      alert("type correct values");
+    } else {
+      alert("all good :)");
+    }
+    e.preventDefault();
+  }
+  function userHandler(e) {
+    let item = e.target.value;
+    if (item.length < 3) {
+      setUserErr(true);
+    } else {
+      setUserErr(false);
+    }
+    setUser(item);
+    console.log(e.target.value.length);
+  }
+  function passwordHandler(e) {
+    let item = e.target.value;
+    if (item.length < 3) {
+      setPassErr(true);
+    } else {
+      setPassErr(false);
+    }
+    setPassword(item);
+    console.log(e.target.value.length);
+  }
+  return (
+    <div>
+      <form onSubmit={loginHandle}>
+        <h1>Login</h1>
+        <input type="text" placeholder="Enter User Id" onChange={userHandler} />
+        {userErr ? <span>User Not Valid</span> : ""}
+        <br /> <br />
+        <input
+          type="password"
+          placeholder="Enter User Password"
+          onChange={passwordHandler}
+        />
+        {passErr ? <span>Password Not Valid</span> : ""}
+        <br /> <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+export default Login;
