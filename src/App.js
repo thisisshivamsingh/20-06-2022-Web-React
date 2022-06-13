dd031f8f19aec528b7af93e0a415e62f128e81cf
Shivam <thisisshivam06@gmail.com>
2022-06-13 13:52:55 +0530

    Component Did Update Life cycle method in React JS

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import React from "react";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { name: "shivam" };
+    console.log("constructor");
+    this.state = {
+      count: 0,
+    };
   }
-  componentDidMount() {
-    console.log("componentDidMount");
+  componentDidUpdate(preProps, preState, snapshot) {
+    // console.log("componentDidUpdate", preState.count, this.state.count);
+    console.log("componentDidUpdate", snapshot);
+    if (this.state.count < 10) {
+      this.setState({ count: this.state.count + 1 });
+    }
   }
   render() {
-    console.log("render");
-    // this.setState({ name: "Singh" });
+    // console.log("render");
     return (
       <div className="App">
-        <h1>Component Did Mount {this.state.name}</h1>
+        <h1>Component Did Update {this.state.count}</h1>
+
         <button
           onClick={() => {
-            this.setState({ name: "Singh" });
+            this.setState({ count: 1 });
           }}
         >
           Update Name
