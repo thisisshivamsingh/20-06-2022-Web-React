49197ff98e2f41f41f9e79dee0496f52c6945ed8
Shivam <thisisshivam06@gmail.com>
2022-06-14 01:01:14 +0530

    Style in React Js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import React, { useState } from "react";
-import User from "./User";
+import "./style.css";
+import style from "./custom.module.css";
+import React from "react";
 function App() {
-  const [data, setData] = useState(10);
-  const [count, setCount] = useState(100);
   return (
     <div className="App">
-      <User count={count} data={data} />
-      <button onClick={() => setCount(count + 1)}>Update Count</button>
-      <button onClick={() => setData(data + 1)}>Update Data</button>
+      <h1 className="primary">Style type 1 in React js</h1>
+      <h1 style={{ color: "red", backgroundColor: "black" }}>
+        Style type 2 in React js
+      </h1>
+      <h1 className={style.success}>Style type 3 in React js</h1>
     </div>
   );
 }
