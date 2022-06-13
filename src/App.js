dbd3a36fee4ac40654603eae266444012a1a85cb
Shivam <thisisshivam06@gmail.com>
2022-06-13 14:44:37 +0530

    Should Component Update Life Cycle Method

--- src/App.js
+++ src/App.js
@@ -3,30 +3,26 @@ import React from "react";
 class App extends React.Component {
   constructor() {
     super();
-    console.log("constructor");
     this.state = {
       count: 0,
     };
   }
-  componentDidUpdate(preProps, preState, snapshot) {
-    // console.log("componentDidUpdate", preState.count, this.state.count);
-    console.log("componentDidUpdate", snapshot);
-    if (this.state.count < 10) {
-      this.setState({ count: this.state.count + 1 });
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate", this.state.count);
+    if (this.state.count > 5 && this.state.count < 10) {
+      return true;
     }
   }
   render() {
-    // console.log("render");
     return (
       <div className="App">
-        <h1>Component Did Update {this.state.count}</h1>
-
+        <h1>Should Component Update {this.state.count}</h1>
         <button
           onClick={() => {
-            this.setState({ count: 1 });
+            this.setState({ count: this.state.count + 1 });
           }}
         >
-          Update Name
+          Update Counter
         </button>
       </div>
     );
