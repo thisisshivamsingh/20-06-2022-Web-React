a51124deb094fd4e4045d7a319f9527ec3a28268
Shivam <thisisshivam06@gmail.com>
2022-06-14 15:27:13 +0530

    Array Listing with Map function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
 import React from "react";
-import { Button, Alert } from "react-bootstrap";
 function App() {
+  // const students = ["Shivam", "Singh", "Sam", "Peter"];
+  const students = [
+    { name: "shivam", email: "shivam@test.com", contact: 888 },
+    { name: "singh", email: "singh@test.com", contact: 111 },
+    { name: "sam", email: "sam@test.com", contact: 222 },
+    { name: "peter", email: "peter@test.com", contact: 333 },
+  ];
   return (
     <div className="App">
-      <h1>Install Bootstrap</h1>
-      {<Alert variant="warning">This is a warning alert—check it out!</Alert>}
+      <h1>Handle Array with List</h1>
+      <table border="1">
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Contact</td>
+        </tr>
+        {students.map((data) => (
+          <tr>
+            <td>{data.name}</td>
+            <td>{data.email}</td>
+            <td>{data.contact}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
