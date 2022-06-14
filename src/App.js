f8f34934cf74a1194f21bcf3129b98d271fe3eeb
Shivam <thisisshivam06@gmail.com>
2022-06-14 23:16:11 +0530

    Fragment in React JS

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import "./App.css";
 import React from "react";
-import User from "./User";
+import Cols from "./Cols";
 function App() {
-  const users = [
-    {
-      name: "Shivam",
-      email: "shivam@test.com",
-      contact: "111",
-    },
-    {
-      name: "Bruce",
-      email: "bruce@test.com",
-      contact: "222",
-    },
-    {
-      name: "Peter",
-      email: "peter@test.com",
-      contact: "333",
-    },
-    {
-      name: "Sam",
-      email: "sam@test.com",
-      contact: "444",
-    },
-  ];
   return (
-    <div className="App">
-      <h1>Reuse Component with List</h1>
-      {users.map((item, i) => (
-        <User data={item} />
-      ))}
+    <div>
+      <h1>React Fragment</h1>
+      <table>
+        <tbody>
+          <tr>
+            <Cols />
+            <Cols />
+            <Cols />
+            <Cols />
+            <Cols />
+            <Cols />
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/Cols.js
@@ -0,0 +1,10 @@
+function Cols() {
+  function Test() {}
+  return (
+    <>
+      <td className="abc">Shivam</td>
+      <td>Singh</td>
+    </>
+  );
+}
+export default Cols;
