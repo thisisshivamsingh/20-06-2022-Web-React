65655d928c2eb52099219d8719c0341e9705f43c
Shivam <thisisshivam06@gmail.com>
2022-06-15 16:12:03 +0530

    Ref with Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import "./App.css";
-import React, { useState, useMemo } from "react";
-function App() {
-  const [count, setCount] = useState(0);
-  const [item, setItem] = useState(10);
-  const multiCountMemo = useMemo(
-    function multiCount() {
-      console.log("multiCount");
-      return count * 5;
-    },
-    [count]
-  );
-  return (
-    <div className="App">
-      <h1>useMemo Hook in React</h1>
-      <h2>Count: {count}</h2>
-      <h2>Item: {item}</h2>
-      <h2>{multiCountMemo}</h2>
-      <button onClick={() => setCount(count + 1)}>Update Count</button>
-      <button onClick={() => setItem(item * 10)}>Update Item</button>
-    </div>
-  );
+import React, { createRef } from "react";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.inputRef = createRef();
+  }
+  componentDidMount() {
+    // console.log((this.inputRef.current.value = "1000"));
+  }
+  getVal() {
+    console.log(this.inputRef.current.value);
+    this.inputRef.current.style.color = "red";
+    this.inputRef.current.style.backgroundColor = "black";
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Ref in React</h1>
+        <input type="text" ref={this.inputRef} />
+        <button onClick={() => this.getVal()}>Check Ref</button>
+      </div>
+    );
+  }
 }
 
 export default App;
