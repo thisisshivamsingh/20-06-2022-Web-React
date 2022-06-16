346004fae2709ffda137edbf37ab1ea2e1ece563
Shivam <thisisshivam06@gmail.com>
2022-06-16 20:09:23 +0530

    Active Link in React Router v6

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,17 +4,35 @@ function NavBar() {
     <div>
       <ul className="navbar">
         <li>
-          <NavLink className="nav-bar-link" to="/">
+          <NavLink
+            style={({ isActive }) => {
+              return { color: isActive ? "skyblue" : "" };
+            }}
+            className="nav-bar-link"
+            to="/"
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink className="nav-bar-link" to="/about">
+          <NavLink
+            style={({ isActive }) => {
+              return { color: isActive ? "skyblue" : "" };
+            }}
+            className="nav-bar-link"
+            to="/about"
+          >
             About
           </NavLink>
         </li>
         <li>
-          <NavLink className="nav-bar-link" to="/contact">
+          <NavLink
+            style={({ isActive }) => {
+              return { color: isActive ? "skyblue" : "" };
+            }}
+            className="nav-bar-link"
+            to="/contact"
+          >
             Contact
           </NavLink>
         </li>
