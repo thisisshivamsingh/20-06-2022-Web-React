5900c024dae24e8fe1bc54bd1b5c0b2e47246cfe
Shivam <thisisshivam06@gmail.com>
2022-06-18 16:15:14 +0530

    React Tutorial Page Not Found - 404 Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Nav from "./Nav";
-import Home from "./Home";
-import About from "./About";
-
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/" exact={true} element={<Home />} />
-      </Routes>
+      <Router>
+        <Link to="/">Home</Link>
+        <br />
+        <br />
+        <Link to="/about">About</Link>
+        <br />
+        <Link to="/login">Login</Link>
+        <br />
+        <a href="/about">About</a>
+        <Routes>
+          <Route path="/" element={<Home />} exact={true} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>This is Home Page</p>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is About Page</p>
+    </div>
+  );
+}
+function PageNotFound() {
+  return (
+    <div>
+      <h1>404 Page</h1>
+      <p>This is Not found</p>
+    </div>
+  );
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <Users /> */}
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
 // ReactDOM.render(
