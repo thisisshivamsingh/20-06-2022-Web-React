99ffb3c7a15a28179c1843bba6bc5f6123e17577
Shivam <thisisshivam06@gmail.com>
2022-06-19 18:06:35 +0530

    React Tutorial POST Method in Rest API

--- src/App.js
+++ src/App.js
@@ -1,35 +1,62 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:3000/users").then((result) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  function saveUser() {
+    console.log({ name, email, mobile });
+    const data = { name, email, mobile };
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((result) => {
+      // console.log("result", result);
       result.json().then((resp) => {
-        // console.log("result", resp);
-        setData(resp);
+        console.log("resp", resp);
       });
     });
-  }, []);
-  console.log(data);
+  }
   return (
     <div className="App">
-      <h1>Get API Call</h1>
-      <table border="1">
-        <tr>
-          <td>ID</td>
-          <td>Name</td>
-          <td>Email</td>
-          <td>Mobile</td>
-        </tr>
-        {data.map((item) => (
-          <tr>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.email}</td>
-            <td>{item.mobile}</td>
-          </tr>
-        ))}
-      </table>
+      <h1>POST API Example</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        name="name"
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        name="name"
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        value={mobile}
+        onChange={(e) => {
+          setMobile(e.target.value);
+        }}
+        name="name"
+      />
+      <br />
+      <br />
+      <button type="button" onClick={saveUser}>
+        Save New User
+      </button>
     </div>
   );
 }
